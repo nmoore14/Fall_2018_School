62e199219894bf6f3c5537d0d97481c655435f4d
Nick Moore <nickmoore_1@live.com>
2018-08-29 19:05:04 -0400

    Adding project folder and first project

---
+++ CISP 1010 - Computer Science 1/Projects/CelsiusToFahrenheit.java
@@ -0,0 +1,26 @@
+/**
+ * CelsiusToFahrenheit
+ * @author nmoore2 (Nicholas Moore)
+ * @category Project 1 (Celsius to Fahrenheit)
+ * @version 1.0
+ * Date: 8-29-2018
+ */
+public class CelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+        // Display what the program is for.
+        // TODO have java display a message saying that this is to convert celsius to fahrenheit
+        System.out.println("Welcome to Celsius to Fahrenheit Convertor.");
+        
+        // Ask the user what degree celsius they would like to convert
+        // TODO create a variable to store the user input
+        // TODO ask the user for their number
+
+        // Create a variable that contains the formula to convert from celsius to fahrenheit
+        // TODO create var tempConversion to store the formula
+
+        // After the conversion is done, display the result
+        // TODO create a message that concatenates the tempConversion variable and a string to disply the results
+    }
+
+}
