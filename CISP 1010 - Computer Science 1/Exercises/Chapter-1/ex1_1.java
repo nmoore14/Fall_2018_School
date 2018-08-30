58dc8c3800e099286ca45cad03a1713a9ae508d1
Nick Moore <nickmoore_1@live.com>
2018-08-30 10:43:21 -0400

    Adding Exercise folders and Chapter 1 exercises

---
+++ CISP 1010 - Computer Science 1/Exercises/Chapter-1/ex1_1.java
@@ -0,0 +1,18 @@
+/**
+ * ex1_1
+ * @author nmoore2 (Nicholas Moore)
+ * 8-30-2018
+ */
+public class ex1_1 {
+
+    public static void main(String[] args) {
+        // Display the message "Welcome to Java"
+        System.out.println("Welcome to Java.");
+
+        // Display the message "Welcome to Computer Science"
+        System.out.println("Welcome to Computer Science.");
+
+        // Display the message "Programming is FUN"
+        System.out.println("Programming is FUN!");
+    }
+}
---
+++ CISP 1010 - Computer Science 1/Exercises/Chapter-1/ex1_2.java
@@ -0,0 +1,16 @@
+/**
+ * ex1_2
+ * @author nmoore2 (Nicholas Moore)
+ * 8-30-2018
+ */
+public class ex1_2 {
+
+    public static void main(String[] args) {
+        // Display the message "Welcome to Java" 5 times
+        int count = 0;
+        while(count < 5) {
+            System.out.println("Welcome to Java.");
+            count++;
+        }
+    }
+}
