ad8f6e006def20fe11faa92f696cad25804f44c3
Nick Moore <nickmoore_1@live.com>
2018-09-20 10:16:54 -0400

    Still going through chapter 14

---
+++ CISP 1010 - Computer Science 1/Exercises/Chapter-14/ButtonInPane.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ButtonInPane extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane pane = new StackPane();
+        pane.getChildren().add(new Button("OK"));
+        Scene scene = new Scene(pane, 200, 50);
+        primaryStage.setTitle("Button In Pane");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
