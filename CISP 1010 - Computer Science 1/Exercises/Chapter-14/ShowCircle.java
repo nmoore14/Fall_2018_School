b1998e6d6505f964b1a6f739da4fba15ef59e14c
Nick Moore <nickmoore_1@live.com>
2018-09-20 11:11:04 -0400

    Still in chapter 14

--- CISP 1010 - Computer Science 1/Exercises/Chapter-14/ShowCircle.java
+++ CISP 1010 - Computer Science 1/Exercises/Chapter-14/ShowCircle.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 public class ShowCircle extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Pane pane = new Pane();
         // Create a circle and set its properties
         Circle circle = new Circle();
-        circle.setCenterX(100);
-        circle.setCenterY(100);
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
         circle.setRadius(50);
         circle.setStroke(Color.BLACK);
         circle.setFill(Color.WHITE);
 
         // Create a pane to hold the circle
-        Pane pane = new Pane();
         pane.getChildren().add(circle);
 
         // Create a scene and place it in the stage
