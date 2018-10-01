ecf0eb8010fc699d9e1fdbf06917a4606e69d7c3
Nick Moore <nickmoore_1@live.com>
2018-10-01 17:16:08 -0400

    Updating the program to use arrays and loops

--- CISP 1010 - Computer Science 1/Projects/NickMoore_Project4_CharacterCalculator.java
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project4_CharacterCalculator.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.lang.Object;
+import java.util.Arrays;
+
 /**
  * -------------------------------------------------
  * File name: NickMoore_Project4_CharacterCalculator.java
@@ -18,7 +21,11 @@ public class NickMoore_Project4_CharacterCalculator
     {
         Scanner input = new Scanner(System.in);
 
-        // TODO Display Welcome message to the user
+        int[] asciiUppercase = {65, 69, 73, 79, 85};
+        int[] asciiLowercase = {97, 101, 105, 111, 117};
+        int[] asciiSpecialChar = {32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 58, 59, 60, 61, 62, 63, 64, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126};
+
+        // Display Welcome message to the user
         System.out.println("====================================================");
         System.out.println("|      ______________________________________      |");
         System.out.println("|         /        Nick Moore Tech       \\         |");
@@ -28,19 +35,54 @@ public class NickMoore_Project4_CharacterCalculator
         System.out.println("|                                                  |");
         System.out.println("====================================================");
         
-        // TODO Ask the user to insert 1 character.
+        // Ask the user to insert 1 character.
         System.out.print("Please enter a single character: ");
         String userInput = input.nextLine();
-        String outputIndent = "        ";
 
-        String userIO = outputIndent + "You entered: " + userInput;
-        System.out.printf("%-10s %s%n", userIO, userIO);
-        // TODO Check to see if the character is an Uppercase or Lowercase vowel.
-        // TODO Output the ASCII value of the character that the user entered.
-        // TODO Check to see if the character that the user entered is an integer.
-        // TODO Check to see if the entered value is a space.
-        // TODO Check to see if the character is a "SPECIAL CHARACTER"
+        // TODO Check to make sure the user entered just 1 character.
+        if (userInput.length() != 1)
+        {
+            System.out.println("Invalid Response.  Must be one character");
+            System.exit(0);
+        } 
+        else
+        {
+            // TODO Output the ASCII value of the character that the user entered.
+            char userChar = userInput.charAt(0);
+            System.out.println(userChar);
+            int ascii = userChar;
+            System.out.println("ASCII: " + ascii);
 
+            // TODO Check to see if the character is an Uppercase or Lowercase vowel.
+            for (int i = 0; i < asciiUppercase.length; i++)
+            {
+                if(ascii == asciiUppercase[i])
+                {
+                    System.out.println("Uppercase Vowel");
+                }
+            }
+
+            for (int i = 0; i < asciiLowercase.length; i++) {
+                if (ascii == asciiLowercase[i]) {
+                    System.out.println("Lowercase Vowel");
+                }
+            }
+
+            // TODO Check to see if the character that the user entered is an integer.
+            if (ascii >= 48 && ascii <= 57)
+            {
+                System.out.println("Character is an interger.");
+            }
+            else
+            {
+                System.out.println("Character is not an integer");
+            }
+
+            // TODO Check to see if the entered value is a space.
+
+            // TODO Check to see if the character is a "SPECIAL CHARACTER"
+
+        }
 
         input.close();
         
