4b61050907e72cb3c25da3f42940e25656b1616e
Nick Moore <nickmoore_1@live.com>
2018-10-03 15:18:28 -0400

    Creating Project 5 -- Java

---
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project5_IntegerValidation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * -------------------------------------------------
+ * File name: NickMoore_Project5_IntegerValidation.java
+ * Project name: Integer Validation
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 10-3-2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_Project5_IntegerValidation
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        input.close();
+    }
+    
+}
