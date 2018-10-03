0ca23182435ad3133de18a04a0ef0ddd1e166701
Nick Moore <nickmoore_1@live.com>
2018-10-03 15:46:34 -0400

    Creating 4b and 4c and finishing them.

---
+++ CISP 1010 - Computer Science 1/Projects/in_Class/NickMoore_inClassEx4b.java
@@ -0,0 +1,29 @@
+ /**
+ * -------------------------------------------------
+ * File name: NickMoore_inClassEx4b.java
+ * Project name: In Class Exercise 4b
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 10-3-2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_inClassEx4b
+{
+    public static void main(String[] args) 
+    {
+        // Displaying my name 5 times using a do-while loop
+        int i = 0;
+
+        do
+        {
+            System.out.println("Nick Moore");
+            i++;
+        }
+        while (i < 5);
+    }
+    
+}
---
+++ CISP 1010 - Computer Science 1/Projects/in_Class/NickMoore_inClassEx4c.java
@@ -0,0 +1,27 @@
+ /**
+ * -------------------------------------------------
+ * File name: NickMoore_inClassEx4c.java
+ * Project name: In Class Exercise 4c
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 10-3-2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_inClassEx4c
+{
+    public static void main(String[] args) 
+    {
+        // Displaying my name 5 times using a for loop
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println("Nick Moore");
+        }
+
+       
+    }
+    
+}
