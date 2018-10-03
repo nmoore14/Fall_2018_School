68e7a8c9801cc9418fc57b0c05c081a44a41e765
Nick Moore <nickmoore_1@live.com>
2018-10-03 15:30:08 -0400

    Creating Header method

--- CISP 1010 - Computer Science 1/Projects/NickMoore_Project5_IntegerValidation.java
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project5_IntegerValidation.java
@@ -15,9 +15,27 @@ import java.util.Scanner;
 
 public class NickMoore_Project5_IntegerValidation
 {
+
+    public static void generateHeader() 
+    {
+        System.out.println("====================================================");
+        System.out.println("|      ______________________________________      |");
+        System.out.println("|         /        Nick Moore Tech       \\         |");
+        System.out.println("|        /         ***PRESENTS***         \\        |");
+        System.out.println("|       /     Integer Input Validation     \\       |");
+        System.out.println("|      /          with Statistics           \\      |");
+        System.out.println("|      ______________________________________      |");
+        System.out.println("|                                                  |");
+        System.out.println("====================================================");
+    }
+
     public static void main(String[] args) 
     {
         Scanner input = new Scanner(System.in);
+
+        // Display the header information
+        generateHeader();
+
         input.close();
     }
     
