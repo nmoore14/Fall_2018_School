b571ffd1116c07306061c1b645f1ba549e24048b
Nick Moore <nickmoore_1@live.com>
2018-10-03 15:43:54 -0400

    Starting and finishing in class ex 4a

---
+++ CISP 1010 - Computer Science 1/Projects/in_Class/NickMoore_inClassEx4a.java
@@ -0,0 +1,28 @@
+ /**
+ * -------------------------------------------------
+ * File name: NickMoore_inClassEx4a.java
+ * Project name: In Class Exercise 4a
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 10-3-2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_inClassEx4a
+{
+    public static void main(String[] args) 
+    {
+        // Displaying my name 5 times using a while loop
+        int i = 0;
+
+        while(i < 5)
+        {
+            System.out.println("Nick Moore");
+            i++;
+        }
+    }
+    
+}
