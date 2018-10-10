adc21fac417ed73019fc6d257ad300cbf01c62e5
Nick Moore <nickmoore_1@live.com>
2018-10-10 19:59:12 -0400

    Adding Project 6 (Java)

---
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project6_DateConverter.java
@@ -0,0 +1,21 @@
+ /**
+ * -------------------------------------------------
+ * File name: NickMoore_Project6_DateConverter.java
+ * Project name: Date Converter
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 10/10/2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_Project6_DateConverter
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
