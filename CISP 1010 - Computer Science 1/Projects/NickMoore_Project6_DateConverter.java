11772097ef21eca9da1b54917b59697b88836c32
Nick Moore <nickmoore_1@live.com>
2018-10-10 20:25:58 -0400

    Starting to program the logic and messages.

--- CISP 1010 - Computer Science 1/Projects/NickMoore_Project6_DateConverter.java
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project6_DateConverter.java
@@ -1,3 +1,5 @@
+ import java.util.*;
+ 
  /**
  * -------------------------------------------------
  * File name: NickMoore_Project6_DateConverter.java
@@ -13,9 +15,52 @@
 
 public class NickMoore_Project6_DateConverter
 {
+    public static void generateHeader() {
+        System.out.println("\t====================================================");
+        System.out.println("\t|      ______________________________________      |");
+        System.out.println("\t|         /        Nick Moore Tech       \\         |");
+        System.out.println("\t|        /         ***PRESENTS***         \\        |");
+        System.out.println("\t|       /          Date Converter          \\       |");
+        System.out.println("\t|      ______________________________________      |");
+        System.out.println("\t|                                                  |");
+        System.out.println("\t====================================================");
+    }
+
+    public static boolean characterCheck(char a) 
+    {
+        int value = a;
+        boolean keepGoing = true;
+
+        if (value == 68 || value == 100) 
+        {
+            keepGoing = false;
+        }
+
+        return keepGoing;
+    }
     public static void main(String[] args) 
     {
+        Scanner input = new Scanner(System.in);
+
+        boolean keepGoing = true;
         
+        // Display the welcome message
+        generateHeader();
+
+        // Loop to keep going until the user enters 'd' or 'D'.
+        while(keepGoing)
+        {
+            // Ask the user for their date.
+            System.out.print("Please enter a date(i.e. 01/01/1989): ");
+
+            // Create a variable to store the users date.
+            String userDate = input.nextLine();
+            System.out.println(userDate);
+            keepGoing = characterCheck(userDate);
+        }
+        
+        // Close the input
+        input.close();
     }
     
 }
