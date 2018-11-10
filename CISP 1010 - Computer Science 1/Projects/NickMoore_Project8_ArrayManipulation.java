100a8269a00d207dfe033a93202e7570173d895f
Nick Moore <nickmoore_1@live.com>
2018-11-10 11:45:48 -0500

    adding the math functions needed

--- CISP 1010 - Computer Science 1/Projects/NickMoore_Project8_ArrayManipulation.java
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project8_ArrayManipulation.java
@@ -29,31 +29,106 @@ public class NickMoore_Project8_ArrayManipulation
 		return going;
 	}
 
-    public static void main(String[] args) 
-    {
-			// Create the new scanner input object
-			Scanner input = new Scanner(System.in);
+	public static boolean checkNumber(String userInputList)
+	{
+		boolean isPosNumber = false;
+		for(int i = 0; i < userInputList.length(); i++)
+		{
+			char a = userInputList.charAt(i);
+			int b = a;
 
-			// Variables
-			boolean keepGoing = true;
-			
-			while(keepGoing)
+			if (b != 32)
 			{
-				// Prompt the user to enter their values for the string array
-				System.out.println("Enter whole, positive integers seperated by a space:");
-				String userInputList = input.nextLine();
-
-				if(userInputList.length() == 1)
+				if (b >= 48 && b <= 57)
 				{
-					keepGoing = checkGoing(userInputList);
+					isPosNumber = true;
+				} 
+				else
+				{
+					isPosNumber = false;
+					break;
+				}
+			} 
+		}
+
+		return isPosNumber;
+	}
+
+	public static Integer[] userArray(String userInputList)
+	{
+		String[] userArray = userInputList.split(" ");
+		Integer[] numArray = new Integer[userArray.length];
+
+		for (int j = 0; j < userArray.length; j++)
+		{
+			numArray[j] = Integer.parseInt(userArray[j]);
+			System.out.println(numArray[j]);
+		}
+		
+		return numArray;
+	}
+
+	public static int findMax(Integer[] userIntArray)
+	{
+		int k = userIntArray.length - 1;
+		int maxNum = userIntArray[k];
+		return maxNum;
+	}
+
+	public static int findMin(Integer[] userIntArray)
+	{
+		int minNum = userIntArray[0];
+
+		return minNum;
+	}
+
+	public static void main(String[] args) 
+	{
+		// Create the new scanner input object
+		Scanner input = new Scanner(System.in);
+
+		// Variables
+		boolean keepGoing = true;
+		boolean isPositiveNumber = false;
+		
+		while(keepGoing)
+		{
+			// Prompt the user to enter their values for the string array
+			System.out.println("Enter whole, positive integers seperated by a space:");
+			String userInputList = input.nextLine();
+
+			if(userInputList.length() == 1)
+			{
+				keepGoing = checkGoing(userInputList);
+			}
+			else
+			{
+				// Check to make sure only whole, positive numbers where entered
+				isPositiveNumber = checkNumber(userInputList);
+				if (isPositiveNumber)
+				{
+					// Convert the user input into an array
+					Integer[] userIntArray = userArray(userInputList);
+					Arrays.sort(userIntArray);
+
+					// Run the array throught the different methods
+					int max = findMax(userIntArray);
+					System.out.printf("Maximum: %s\n", max);
+					
+					int min = findMin(userIntArray);
+					System.out.printf("Minimum: %s\n", min);
+
+					System.out.printf("Ordered Array: %s\n", Arrays.toString(userIntArray));
 				}
 				else
 				{
-					System.out.println(userInputList);
+					System.out.println("Invalid number entry, try again");
 				}
+				
 			}
-			System.out.println("Thank your for playing.");
-			input.close();
-    }
+		}
+		System.out.println("Thank your for playing.");
+		input.close();
+	}
     
 }
