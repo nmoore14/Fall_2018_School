b435f0c27cda3781910c516a2ee8bba180ad6cc4
Nick Moore <nickmoore_1@live.com>
2018-11-14 16:51:57 -0500

    adding project 9 connect 4

---
+++ CISP 1010 - Computer Science 1/Projects/side_projects/NickMoore_Project9_Connect4.java
@@ -0,0 +1,21 @@
+ /**
+ * -------------------------------------------------
+ * File name: NickMoore_Project9_Connect4.java
+ * Project name: Project 9 Connect 4
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 11/14/18
+ * -------------------------------------------------
+ * 
+ */
+
+public class NickMoore_Project9_Connect4
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
