f47d07536f239a181ea222cc7a30f18caf9a356f
Nick Moore <nickmoore_1@live.com>
2018-11-15 11:23:37 -0500

    making the whole row clickable

--- WEBT 2300 - Client Side Programming/Projects/Project4-data/index.js
+++ WEBT 2300 - Client Side Programming/Projects/Project4-data/index.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-	// http://apps.northeaststate.edu/webservices/faculty_staff_directory/index.php?callback=?&format=json
-	// load the json file (linked in the comment above this line) and parse out the table then append to the id faculty_staff_directory
-	// use the 1_jQuery_JSON.html example file from class to reference in completeing this assignment
-});  
-
 var app = new Vue({
 	el: "#app",
 	data: {
