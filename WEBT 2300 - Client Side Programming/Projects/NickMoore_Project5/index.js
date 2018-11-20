cad83c23b6f3b322d98533319ae4f396f8119394
Nick Moore <nickmoore_1@live.com>
2018-11-20 18:06:01 -0500

    Adding Project 5

---
+++ WEBT 2300 - Client Side Programming/Projects/NickMoore_Project5/index.js
@@ -0,0 +1,40 @@
+var app = new Vue({
+	el: "#app",
+	data: {
+		staffDir: [],
+		staffSelect: false,
+		staffName: '',
+		staffTitle: '',
+		staffEct: '',
+		staffLocation: '',
+		staffEmail: '',
+		staffPhone: ''
+	},
+	mounted() {
+		this.getStaff()
+	},
+	methods: {
+		getStaff() {
+			var vm = this
+			axios.get('./data.json')
+				.then(function (res) {
+					vm.staffDir = JSON.parse(JSON.stringify(res.data.staff))
+					vm.renderPage = true
+				})
+				.catch(function (err) {
+					console.log(err)
+				})
+		},
+		selectStaff(id) {
+			console.log(id);
+			this.staffSelect = true;
+			this.staffName = this.staffDir[id].name;
+			this.staffTitle = this.staffDir[id].title;
+			this.staffExt = this.staffDir[id].ext;
+			this.staffLocation = this.staffDir[id].location;
+			this.staffEmail = this.staffDir[id].email;
+			this.staffPhone = this.staffDir[id].phone;
+
+		}
+	}
+})
