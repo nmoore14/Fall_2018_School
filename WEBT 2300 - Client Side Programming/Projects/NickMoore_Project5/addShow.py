c12a58938dab6c464335a1b7b69d55b9c9b751df
Nick Moore <nickmoore_1@live.com>
2018-11-26 20:26:37 -0500

    still working on python

--- WEBT 2300 - Client Side Programming/Projects/NickMoore_Project5/addShow.py
+++ WEBT 2300 - Client Side Programming/Projects/NickMoore_Project5/addShow.py
@@ -1,32 +1,27 @@
 import json
 
 my_dictionary = []
-temp_dict = {}
+temp_dict = []
 
 with open('staff-index.json') as json_file:
-    data = json.load(json_file)
-    for p in data['staff']:
-			print(p)
-			# print('id: ' + p['id'])
-			temp_dict['id'] = p['id']
-			# print('name: ' + p['name'])
-			temp_dict['name'] = p['name']
-			# print('title: ' + p['title'])
-			temp_dict['title'] = p['title']
-			# print('ext: ' + p['ext'])
-			temp_dict['ext'] = p['ext']
-			# print('location: ' + p['location'])
-			temp_dict['location'] = p['location']
-			# print('email: ' + p['email'])
-			temp_dict['email'] = p['email']
-			# print('phone: ' + p['phone'])
-			temp_dict['phone'] = p['phone']
-			# print('')
-			temp_dict['show'] = 'true'
-			my_dictionary.append(temp_dict)
+	data = json.load(json_file)
+	for p in data['staff']:
+		print(p)
+		# temp_dict['id'] = p['id']
+		# temp_dict['name'] = p['name']
+		# temp_dict['title'] = p['title']
+		# temp_dict['ext'] = p['ext']
+		# temp_dict['location'] = p['location']
+		# temp_dict['email'] = p['email']
+		# temp_dict['phone'] = p['phone']
+		# temp_dict['show'] = 'true'
+		temp_dict.append(p)
 
-with open('new-staff.json', 'w') as outfile:
-    json.dump(my_dictionary, outfile)
+# with open('new-staff.json', 'w') as outfile:
+#     json.dump(my_dictionary, outfile)
 
-print(json.dumps(my_dictionary, indent=2))
+# print(json.dumps(temp_dict, indent=2))
+
+print temp_dict[0]
+
 
