a4ef4d2914ae90300d412579fb42f725a0b0daba
Nick Moore <nickmoore_1@live.com>
2018-11-28 16:48:09 -0500

    creating project 10 files

---
+++ CISP 1010 - Computer Science 1/Projects/NickMoore_Project10/shipDesk.java
@@ -0,0 +1,21 @@
+/**
+ * -------------------------------------------------
+ * File name: shipDesk.java
+ * Project name: Project 10 Ship Desk
+ * -------------------------------------------------
+ * Creator's name: Nick Moore
+ * Email: nmoore2@northeaststate.edu
+ * Course and section: CISP 1010 A03
+ * Creation date: 11-28-2018
+ * -------------------------------------------------
+ * 
+ */
+
+public class shipDesk
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
